022ff45363b6097b3a444f1980bb4eaaebceedf7
ngohungphuc <ngohungphuc95@gmail.com>
2018-01-09 14:22:43 +0700

    add admin and client module

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Client/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCMSCore.Modules.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
