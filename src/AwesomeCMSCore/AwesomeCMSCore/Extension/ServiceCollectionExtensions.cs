65a9e0f049369ce05959d9c336d203f99c76a894
ngohungphuc <ngohungphuc95@gmail.com>
2018-01-23 11:25:04 +0700

    update repo

--- src/AwesomeCMSCore/AwesomeCMSCore/Extension/ServiceCollectionExtensions.cs
+++ src/AwesomeCMSCore/AwesomeCMSCore/Extension/ServiceCollectionExtensions.cs
@@ -16,6 +16,10 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+using Microsoft.DotNet.PlatformAbstractions;
 
 namespace AwesomeCMSCore.Extension
 {
@@ -74,11 +78,11 @@ namespace AwesomeCMSCore.Extension
             var mvcBuilder = services
                 .AddMvc()
                 // fix assembly ref for web api
-                .ConfigureApplicationPartManager(manager =>
-                {
-                    manager.FeatureProviders.Remove(manager.FeatureProviders.First(f => f is MetadataReferenceFeatureProvider));
-                    manager.FeatureProviders.Add(new ReferencesMetadataReferenceFeatureProvider());
-                })
+                //.ConfigureApplicationPartManager(manager =>
+                //{
+                //    manager.FeatureProviders.Remove(manager.FeatureProviders.First(f => f is MetadataReferenceFeatureProvider));
+                //    manager.FeatureProviders.Add(new ReferencesMetadataReferenceFeatureProvider());
+                //})
                 .AddRazorOptions(o =>
                 {
                     foreach (var module in modules)
@@ -154,4 +158,62 @@ namespace AwesomeCMSCore.Extension
             return services;
         }
     }
+
+    //public class MvcConfiguration : IDesignTimeMvcBuilderConfiguration
+    //{
+    //    private class DirectReferenceAssemblyResolver : ICompilationAssemblyResolver
+    //    {
+    //        public bool TryResolveAssemblyPaths(CompilationLibrary library, List<string> assemblies)
+    //        {
+    //            if (!string.Equals(library.Type, "reference", StringComparison.OrdinalIgnoreCase))
+    //            {
+    //                return false;
+    //            }
+
+    //            var paths = new List<string>();
+
+    //            foreach (var assembly in library.Assemblies)
+    //            {
+    //                var path = Path.Combine(ApplicationEnvironment.ApplicationBasePath, assembly);
+
+    //                if (!File.Exists(path))
+    //                {
+    //                    return false;
+    //                }
+
+    //                paths.Add(path);
+    //            }
+
+    //            assemblies.AddRange(paths);
+
+    //            return true;
+    //        }
+    //    }
+
+    //    public void ConfigureMvc(IMvcBuilder builder)
+    //    {
+    //        // .NET Core SDK v1 does not pick up reference assemblies so
+    //        // they have to be added for Razor manually. Resolved for
+    //        // SDK v2 by https://github.com/dotnet/sdk/pull/876 OR SO WE THOUGHT
+    //        /*builder.AddRazorOptions(razor =>
+    //        {
+    //            razor.AdditionalCompilationReferences.Add(
+    //                MetadataReference.CreateFromFile(
+    //                    typeof(PdfHttpHandler).Assembly.Location));
+    //        });*/
+
+    //        // .NET Core SDK v2 does not resolve reference assemblies' paths
+    //        // at all, so we have to hack around with reflection
+    //        typeof(CompilationLibrary)
+    //            .GetTypeInfo()
+    //            .GetDeclaredField("<DefaultResolver>k__BackingField")
+    //            .SetValue(null, new CompositeCompilationAssemblyResolver(new ICompilationAssemblyResolver[]
+    //            {
+    //                new DirectReferenceAssemblyResolver(),
+    //                new AppBaseCompilationAssemblyResolver(),
+    //                new ReferenceAssemblyPathResolver(),
+    //                new PackageCompilationAssemblyResolver(),
+    //            }));
+    //    }
+    //}
 }
--- src/AwesomeCMSCore/AwesomeCMSCore/Gruntfile.js
+++ src/AwesomeCMSCore/AwesomeCMSCore/Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function (grunt) {
                     "../Modules/**/Views/**",
                     "../Modules/**/bin/Debug/**/**/*.*",
                     "../Modules/**/wwwroot/**",
-                    "!../Modules/AwesomeCMSCore.Modules.Console/**"
+                    "!../Modules/AwesomeCMSCore.Modules.Console/**",
+                    "!../Modules/AwesomeCMSCore.Modules.Frontend/**"
                 ],
                 dest: "./Modules/"
             },
