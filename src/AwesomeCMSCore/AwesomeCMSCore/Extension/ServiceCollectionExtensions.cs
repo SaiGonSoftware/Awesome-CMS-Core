bb1b423b71f1eae48774caf18df855ce7536a776
ngohungphuc <ngohungphuc95@gmail.com>
2018-01-23 11:41:45 +0700

    Fix compliation error

--- src/AwesomeCMSCore/AwesomeCMSCore/Extension/ServiceCollectionExtensions.cs
+++ src/AwesomeCMSCore/AwesomeCMSCore/Extension/ServiceCollectionExtensions.cs
@@ -77,12 +77,6 @@ namespace AwesomeCMSCore.Extension
         {
             var mvcBuilder = services
                 .AddMvc()
-                // fix assembly ref for web api
-                //.ConfigureApplicationPartManager(manager =>
-                //{
-                //    manager.FeatureProviders.Remove(manager.FeatureProviders.First(f => f is MetadataReferenceFeatureProvider));
-                //    manager.FeatureProviders.Add(new ReferencesMetadataReferenceFeatureProvider());
-                //})
                 .AddRazorOptions(o =>
                 {
                     foreach (var module in modules)
@@ -158,62 +152,4 @@ namespace AwesomeCMSCore.Extension
             return services;
         }
     }
-
-    //public class MvcConfiguration : IDesignTimeMvcBuilderConfiguration
-    //{
-    //    private class DirectReferenceAssemblyResolver : ICompilationAssemblyResolver
-    //    {
-    //        public bool TryResolveAssemblyPaths(CompilationLibrary library, List<string> assemblies)
-    //        {
-    //            if (!string.Equals(library.Type, "reference", StringComparison.OrdinalIgnoreCase))
-    //            {
-    //                return false;
-    //            }
-
-    //            var paths = new List<string>();
-
-    //            foreach (var assembly in library.Assemblies)
-    //            {
-    //                var path = Path.Combine(ApplicationEnvironment.ApplicationBasePath, assembly);
-
-    //                if (!File.Exists(path))
-    //                {
-    //                    return false;
-    //                }
-
-    //                paths.Add(path);
-    //            }
-
-    //            assemblies.AddRange(paths);
-
-    //            return true;
-    //        }
-    //    }
-
-    //    public void ConfigureMvc(IMvcBuilder builder)
-    //    {
-    //        // .NET Core SDK v1 does not pick up reference assemblies so
-    //        // they have to be added for Razor manually. Resolved for
-    //        // SDK v2 by https://github.com/dotnet/sdk/pull/876 OR SO WE THOUGHT
-    //        /*builder.AddRazorOptions(razor =>
-    //        {
-    //            razor.AdditionalCompilationReferences.Add(
-    //                MetadataReference.CreateFromFile(
-    //                    typeof(PdfHttpHandler).Assembly.Location));
-    //        });*/
-
-    //        // .NET Core SDK v2 does not resolve reference assemblies' paths
-    //        // at all, so we have to hack around with reflection
-    //        typeof(CompilationLibrary)
-    //            .GetTypeInfo()
-    //            .GetDeclaredField("<DefaultResolver>k__BackingField")
-    //            .SetValue(null, new CompositeCompilationAssemblyResolver(new ICompilationAssemblyResolver[]
-    //            {
-    //                new DirectReferenceAssemblyResolver(),
-    //                new AppBaseCompilationAssemblyResolver(),
-    //                new ReferenceAssemblyPathResolver(),
-    //                new PackageCompilationAssemblyResolver(),
-    //            }));
-    //    }
-    //}
 }
