2d361331cf0766b2aa87f73e4ff529aee0d76151
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-09 00:23:47 +0700

    done to get token

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Frontend/wwwroot/js/React/Account/LoginForm.js
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Frontend/wwwroot/js/React/Account/LoginForm.js
@@ -15,25 +15,67 @@ import axios from "axios";
 class LoginForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: "", password: "", error: "", loading: false };
+    this.state = {
+      username: "",
+      password: "",
+      error: "",
+      loading: false
+    };
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   login(event) {
     event.preventDefault();
-    console.log(this.state.username);
-    console.log(this.state.password);
-    let data = JSON.stringify({
-      username: this.state.username,
-      password: this.state.password
-    });
 
-    axios.post("https://localhost:5000//connect/token", data, {
+    let data = {
+      username: this.state.username,
+      password: this.state.password,
+      grant_type: "password",
+      scope: "offline_access"
+    };
+
+    let config = {
       headers: {
-        grant_type: "password",
-        scope: "offline_access",
         "Content-Type": "application/x-www-form-urlencoded"
       }
-    });
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5000/connect/token");
+    var dataa = {
+      username: this.state.username,
+      password: this.state.password,
+      grant_type: "password",
+      // specify the resource, to match the audience in the jwt bearer middleware
+      resource: "http://localhost:5000/",
+      // offline_access: indicate refresh token is required
+      // profile: include custom fields
+      // email: include email address
+      scope: "offline_access"
+    };
+    var body = "";
+    for (var key in dataa) {
+      if (body.length) {
+        body += "&";
+      }
+      body += key + "=";
+      body += encodeURIComponent(data[key]);
+    }
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(body);
+    /* axios
+      .post("http://localhost:5000/connect/token", data, config)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error.response);
+      }); */
   }
 
   render() {
@@ -43,7 +85,7 @@ class LoginForm extends Component {
           <Col md="12" id="loginContainer">
             <Form id="loginForm" onSubmit={this.login.bind(this)}>
               <div className="panel-heading">
-                <h3 className="panel-title">Admin portal</h3>
+                <h3 className="panel-title"> Admin portal </h3>
               </div>
               <div id="loginFormContent">
                 <FormGroup>
@@ -55,7 +97,8 @@ class LoginForm extends Component {
                     name="username"
                     id="username"
                     placeholder="Username"
-                    onChange={username => this.setState({ username })}
+                    value={this.state.username}
+                    onChange={username => this.onChange(username)}
                   />
                 </FormGroup>
                 <FormGroup>
@@ -68,7 +111,7 @@ class LoginForm extends Component {
                     id="password"
                     placeholder="Password"
                     value={this.state.password}
-                    onChange={password => this.setState({ password })}
+                    onChange={username => this.onChange(username)}
                   />
                 </FormGroup>
                 <Button color="primary" type="submit">
