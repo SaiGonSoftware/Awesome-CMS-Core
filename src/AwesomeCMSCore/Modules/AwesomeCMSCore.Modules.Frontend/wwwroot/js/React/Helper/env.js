98b6392f0b41e871231a179d8e8fa04637715f41
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-19 22:47:52 +0700

    add node env config

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Frontend/wwwroot/js/React/Helper/env.js
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Frontend/wwwroot/js/React/Helper/env.js
@@ -1,6 +1,14 @@
-const env = {
+let env = {};
+
+if (process.env.NODE_ENV === "production") {
+  env = {
+    baseUrl: "http://randomshit.com",
+    authorizeUrl: "http://randomshit.com/connect/token"
+  };
+} else {
+  env = {
     baseUrl: "http://localhost:5000/",
     authorizeUrl: "http://localhost:5000/connect/token"
-};
-
+  };
+}
 export default env;
