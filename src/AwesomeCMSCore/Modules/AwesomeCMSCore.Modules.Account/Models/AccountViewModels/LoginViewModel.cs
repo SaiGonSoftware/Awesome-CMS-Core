804791550ff1b2973b9a9695a6cc50cfd60e3e96
ngohungphuc <ngohungphuc95@gmail.com>
2018-04-04 13:06:04 +0700

    add account module

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Account/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwesomeCMSCore.Modules.Account.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
