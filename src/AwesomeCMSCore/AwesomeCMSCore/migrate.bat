ff60fb516b63e138dfd351f55797d0df0fed4f37
ngohungphuc <ngohungphuc95@gmail.com>
2018-04-04 11:27:44 +0700

    update batch script

--- src/AwesomeCMSCore/AwesomeCMSCore/migrate.bat
+++ src/AwesomeCMSCore/AwesomeCMSCore/migrate.bat
@@ -1,4 +1,26 @@
+@ECHO OFF
+CLS
+ECHO ====== AWESOME CMS CORE =======
+ECHO 1.Add new migrate
+ECHO 2.Update database
+ECHO ====== AWESOME CMS CORE =======
+ECHO.
+
+CHOICE /C 12 /M "Enter your choice:"
+IF ERRORLEVEL 2 GOTO Update database
+IF ERRORLEVEL 1 GOTO Add new migrate
+
+:Add new migrate
+ECHO Add new migrate
 set t=%date%_%time%
 set d=%t:~10,4%%t:~7,2%%t:~4,2%_%t:~15,2%%t:~18,2%%t:~21,2%
 dotnet ef migrations add InitDB_%d% --verbose
 dotnet ef database update --verbose
+GOTO End
+
+:Update database
+dotnet ef database update --verbose
+GOTO End
+
+:End
+@pause
