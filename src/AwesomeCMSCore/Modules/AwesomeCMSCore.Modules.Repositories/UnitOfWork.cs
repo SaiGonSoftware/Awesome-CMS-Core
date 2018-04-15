e836ccc25fce37e89162a136a353c887805e4354
Tony Hudson <ngohungphuc@users.noreply.github.com>
2018-04-15 10:27:44 +0700

    Update UnitOfWork.cs (#108)

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Repositories/UnitOfWork.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Repositories/UnitOfWork.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +11,11 @@ namespace AwesomeCMSCore.Modules.Repositories
         private readonly ApplicationDbContext _dbContext;
         private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
 
-        public Dictionary<Type, object> Repositories { get; set; }
+        public Dictionary<Type, object> Repositories
+        {
+            get { return _repositories; }
+            set { Repositories = value; }
+        }
 
         public UnitOfWork(ApplicationDbContext dbContext)
         {
@@ -32,7 +36,7 @@ namespace AwesomeCMSCore.Modules.Repositories
 
         public async Task<int> Commit()
         {
-           return await _dbContext.SaveChangesAsync();
+            return await _dbContext.SaveChangesAsync();
         }
 
         public void Rollback()
