b7de32abb8a359cbd53a3d1c3f1a7c5deb23f3bb
ngohungphuc <ngohungphuc95@gmail.com>
2018-04-19 12:12:51 +0700

    add styling for checkbox

--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Common/ACCInput/ACCCheckbox.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Common/ACCInput/ACCCheckbox.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ACCCheckbox = props => {
+  const styleClass =
+    props.index % 2 === 0
+      ? "form-group custom-checkbox alignleft"
+      : "form-group custom-checkbox alignright";
+
   return (
     <div className="form-group custom-checkbox">
       <div className="custom-control custom-checkbox">
@@ -24,7 +29,8 @@ ACCCheckbox.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  index: PropTypes.number
 };
 
 export default ACCCheckbox;
