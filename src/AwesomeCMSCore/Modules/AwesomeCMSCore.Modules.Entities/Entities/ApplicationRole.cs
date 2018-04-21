ae1fd32b136a0c0f83dd6fdeaf2d6f52463a3967
Saravanan <saravanan.csengg@gmail.com>
2018-04-21 22:35:50 +0800

    Code reviewed

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Entities/Entities/ApplicationRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCMSCore.Modules.Entities.Entities
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+        public virtual ICollection<IdentityUserRole<string>> Users { get; set; }
+
+        public virtual ICollection<IdentityRoleClaim<string>> Claims { get; set; }
+    }
+}
