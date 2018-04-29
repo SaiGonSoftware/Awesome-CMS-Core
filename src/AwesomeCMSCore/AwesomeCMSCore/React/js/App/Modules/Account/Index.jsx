f4b5ac0a45cca6ce85eaee78cc13c6d5b6d99ef7
Saravanan <saravanan.csengg@gmail.com>
2018-04-29 14:47:26 +0800

    merge from master

--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Account/Index.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Account/Index.jsx
@@ -56,13 +56,10 @@ class AccountTable extends Component {
 
   toggleAccountStatus = () => {
     if (this.state.selectedId) {
-      Post(
-        env.deactiveAccount,
-        {
-          AccountId: this.state.selectedId,
-          ToogleFlag: this.state.toogleFlag
-        }
-      )
+      Post(env.deactiveAccount, {
+        AccountId: this.state.selectedId,
+        ToogleFlag: this.state.toogleFlag
+      })
         .then(() => {
           toastr.info("Account status successfully set");
         })
@@ -159,8 +156,8 @@ class AccountTable extends Component {
             >
               Email Confirmed
             </TableHeaderColumn>
-            <TableHeaderColumn dataField="groups">Groups</TableHeaderColumn>
-          </BootstrapTable>,
+            <TableHeaderColumn dataField="roles">Roles</TableHeaderColumn>
+          </BootstrapTable>
         </div>
       </div>
     );
--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Account/Manage/AddUserModal.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Account/Manage/AddUserModal.jsx
@@ -3,15 +3,15 @@ import PropTypes from "prop-types";
 import toastr from "toastr";
 
 import { shouldMarkError, validateInput } from "../../../Helper/Validation";
-import { onChange, onBlur } from "../../../Helper/stateHelper";
-import { Get, PostWithSpinner } from "../../../Helper/ajax";
+import { onChange } from "../../../Helper/stateHelper";
+import { Get, PostWithSpinner, Post } from "../../../Helper/ajax";
 import { isFormValid } from "../../../Helper/Validation";
 import env from "../../../Helper/envConfig";
 import statusCode from "../../../Helper/StatusCode";
 
 import ACCInput from "../../../Common/ACCInput/ACCInput.jsx";
-import ACCButton from "../../../Common/ACCButton.jsx";
 import ACCMultiCheckbox from "../../../Common/ACCInput/ACCMultiCheckbox.jsx";
+import Spinner from "../../../Common/Spinner.jsx";
 
 class AddUserModal extends Component {
   constructor(props) {
@@ -21,6 +21,8 @@ class AddUserModal extends Component {
       email: "",
       roleList: [],
       loading: false,
+      duplicateUserName: false,
+      duplicateEmail: false,
       touched: {
         username: false,
         email: false
@@ -47,29 +49,94 @@ class AddUserModal extends Component {
     }
   };
 
+  onBlur(e) {
+    this.setState({
+      touched: { ...this.state.touched, [e.target.name]: true }
+    });
+
+    if (e.target.name === "username") {
+      Post(env.validateDuplicateAccountInfo, {
+        Key: "UserName",
+        Value: this.state.username
+      }).then(res => {
+        res.data
+          ? this.setState({ duplicateUserName: true })
+          : this.setState({ duplicateUserName: false });
+      });
+    }
+
+    if (e.target.name === "email") {
+      Post(env.validateDuplicateAccountInfo, {
+        Key: "Email",
+        Value: this.state.email
+      }).then(res => {
+        res.data
+          ? this.setState({ duplicateEmail: true })
+          : this.setState({ duplicateEmail: false });
+      });
+    }
+  }
+
   addNewUser = e => {
-    if (!isFormValid(this.validationArr)) {
+    if (
+      !isFormValid(this.validationArr) ||
+      this.state.duplicateEmail ||
+      this.state.duplicateUserName
+    ) {
       return;
     }
 
     e.preventDefault();
 
-    PostWithSpinner.call(this, env.addNewUser, {
-      Username: this.state.username,
-      Email: this.state.email,
-      Roles: [...this.selectedRoles]
-    })
-      .then(res => {
-        if (res.status === statusCode.Success)
-          toastr.info("Account successfully create");
+    if (this.state.duplicateEmail || this.state.duplicateUserName) {
+      PostWithSpinner.call(this, env.addNewUser, {
+        Username: this.state.username,
+        Email: this.state.email,
+        Roles: [...this.selectedRoles]
       })
-      .catch(() => {
-        toastr.error("Something went wrong. Please try again");
-      });
+        .then(res => {
+          if (res.status === statusCode.Success)
+            toastr.info("Account successfully create");
+        })
+        .catch(() => {
+          toastr.error("Something went wrong. Please try again");
+        });
+    }
   };
 
+  renderButton() {
+    const errors = validateInput(this.validationArr);
+    const inputErrors = Object.keys(errors).some(x => errors[x]);
+    let isDisabled = false;
+
+    if (
+      inputErrors ||
+      this.state.duplicateEmail ||
+      this.state.duplicateUserName
+    ) {
+      isDisabled = true;
+    }
+
+    if (this.state.loading) {
+      return <Spinner />;
+    } else {
+      return (
+        <button className="btn btn-primary" type="submit" disabled={isDisabled}>
+          Save
+        </button>
+      );
+    }
+  }
+
   render() {
-    const { username, email, loading, roleList } = this.state;
+    const {
+      username,
+      email,
+      roleList,
+      duplicateUserName,
+      duplicateEmail
+    } = this.state;
+
     this.validationArr = [
       {
         username,
@@ -87,76 +154,81 @@ class AddUserModal extends Component {
         aria-labelledby={this.props.id}
         aria-hidden="true"
       >
-        <form onSubmit={this.addNewUser}>
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title"> Add new account </h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <ACCInput
-                  className={shouldMarkError.call(this, "username", errors)}
-                  type="text"
-                  name="username"
-                  id="username"
-                  placeholder="Username"
-                  required="required"
-                  value={username}
-                  onChange={username => onChange.call(this, username)}
-                  onBlur={username => onBlur.call(this, username)}
-                />
-                <ACCInput
-                  className={shouldMarkError.call(this, "email", errors)}
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  required="required"
-                  value={email}
-                  onChange={email => onChange.call(this, email)}
-                  onBlur={email => onBlur.call(this, email)}
-                />
-                <div className="card" id="userRoleSection">
-                  <div className="card-body">
-                    <h5 className="card-title">Roles</h5>
-                    {roleList.map((role, index) => (
-                      <ACCMultiCheckbox
-                        index={index}
-                        key={role.id}
-                        id={role.id}
-                        name={role.name}
-                        label={role.name}
-                        handleCheckboxChange={this.onSelectRoles}
-                      />
-                    ))}
-                  </div>
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title"> Add new account </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              {duplicateUserName ? (
+                <div className="alert alert-danger" role="alert">
+                  UserName is duplicate
+                </div>
+              ) : null}
+              {duplicateEmail ? (
+                <div className="alert alert-danger" role="alert">
+                  Email is duplicate
+                </div>
+              ) : null}
+
+              <ACCInput
+                className={shouldMarkError.call(this, "username", errors)}
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+                required="required"
+                value={username}
+                onChange={username => onChange.call(this, username)}
+                onBlur={username => this.onBlur(username)}
+              />
+              <ACCInput
+                className={shouldMarkError.call(this, "email", errors)}
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required="required"
+                value={email}
+                onChange={email => onChange.call(this, email)}
+                onBlur={email => this.onBlur(email)}
+              />
+              <div className="card" id="userRoleSection">
+                <div className="card-body">
+                  <h5 className="card-title">Roles</h5>
+                  {roleList.map((role, index) => (
+                    <ACCMultiCheckbox
+                      index={index}
+                      key={role.id}
+                      id={role.id}
+                      name={role.name}
+                      label={role.name}
+                      handleCheckboxChange={this.onSelectRoles}
+                    />
+                  ))}
                 </div>
               </div>
-              <div className="modal-footer">
-                <ACCButton
-                  validationArr={this.validationArr}
-                  loading={loading}
-                  label="Add"
-                />
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
-              </div>
+            </div>
+            <div className="modal-footer">
+              {this.renderButton()}
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     );
   }
