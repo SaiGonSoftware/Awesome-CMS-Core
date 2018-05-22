08b172249e9116b90e381ff9f1410c8365a28c26
Tony Hudson <ngohungphuc95@gmail.com>
2018-05-23 00:58:31 +0700

    update

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Account/Repositories/IAccountRepository.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Account/Repositories/IAccountRepository.cs
@@ -14,6 +14,5 @@ namespace AwesomeCMSCore.Modules.Account.Repositories
         Task<bool> ValidateDuplicateAccountInfo(UserAccountValidateObject accountValidateObject);
         Task<RolesUserViewModel> GetUserRolesById(string userId);
         Task<bool> EditUserRoles(RolesUserViewModel rolesUserVm);
-        
     }
 }
