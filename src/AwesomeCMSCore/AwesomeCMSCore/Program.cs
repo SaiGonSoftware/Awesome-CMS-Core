8aecdc0817cfbb94ecfbf35845552a51e77780f4
ngohungphuc <ngohungphuc95@gmail.com>
2018-06-18 10:27:35 +0700

    update

--- src/AwesomeCMSCore/AwesomeCMSCore/Program.cs
+++ src/AwesomeCMSCore/AwesomeCMSCore/Program.cs
@@ -9,28 +9,19 @@ namespace AwesomeCMSCore
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                SeedData.Initialize(services).Wait();
+            }
+
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-
-        //public static void Main(string[] args)
-        //{
-        //    var host = BuildWebHost(args);
-        //    using (var scope = host.Services.CreateScope())
-        //    {
-        //        var services = scope.ServiceProvider;
-        //        SeedData.Initialize(services).Wait();
-        //    }
-
-        //    host.Run();
-        //}
-
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>()
-        //        .Build();
+                .UseStartup<Startup>()
+                .Build();
     }
 }
