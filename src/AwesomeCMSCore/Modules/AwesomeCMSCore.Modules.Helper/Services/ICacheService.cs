86f7db88b40106e9a1b19db709bbd15fa1df35bb
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-08 20:46:50 +0700

    add interface to interact with redis

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Helper/Services/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AwesomeCMSCore.Modules.Helper.Services
+{
+    public interface ICacheService: IDistributedCache
+    {
+    }
+}
