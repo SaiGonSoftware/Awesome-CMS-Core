a24ecb9b4dfc5eeeee02fba105e165b66f138d20
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-08 20:44:52 +0700

    integrate redis

--- src/AwesomeCMSCore/AwesomeCMSCore/Startup.cs
+++ src/AwesomeCMSCore/AwesomeCMSCore/Startup.cs
@@ -47,6 +47,7 @@ namespace AwesomeCMSCore
             services.ConfigApiVersioning();
             services.IntegrateSwagger();
             services.RegisterGzip();
+            services.IntegrateRedis(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
