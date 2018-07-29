abb95c93931d985c4b8e642bf10383ad5e586a35
Tony Hudson <ngohungphuc@users.noreply.github.com>
2018-07-29 18:57:43 +0700

    Add post status enum (#190)

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Entities/Enums/PostStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCMSCore.Modules.Entities.Enums
+{
+    public enum PostStatus
+    {
+        Published = 1,
+        Draft = 2,
+        Deleted = 3
+    }
+}
