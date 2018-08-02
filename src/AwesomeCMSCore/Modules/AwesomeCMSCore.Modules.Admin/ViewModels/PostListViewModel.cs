1637e6399f00df4d880bd713ee6d42680bb8acc5
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-02 15:10:58 +0700

    update view model

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/PostListViewModel.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/PostListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using AwesomeCMSCore.Modules.Entities.Enums;
 
 namespace AwesomeCMSCore.Modules.Admin.ViewModels
 {
@@ -8,7 +10,17 @@ namespace AwesomeCMSCore.Modules.Admin.ViewModels
         public string Title { get; set; }
         public string ShortDescription { get; set; }
         public string Content { get; set; }
-        public bool IsDeleted { get; set; }
+        public PostStatus PostStatus { get; set; }
         public DateTime DateCreated { get; set; }
+    }
+
+    public class PostDefaultViewModel
+    {
+        public IEnumerable<PostListViewModel> PostsPublished { get; set; }
+        public IEnumerable<PostListViewModel> PostsDrafted { get; set; }
+        public IEnumerable<PostListViewModel> Postseleted { get; set; }
+        public int NumberOfPostPublished { get; set; }
+        public int NumberOfDraftedPost { get; set; }
+        public int NumberOfDeletedPost { get; set; }
     }
 }
