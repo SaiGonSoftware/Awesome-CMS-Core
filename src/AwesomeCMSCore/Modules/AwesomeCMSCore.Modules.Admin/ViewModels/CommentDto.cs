b75f3ecc42ba4daf1b11fd6b8dca4a8426c87a48
Tony Hudson <ngohungphuc95@gmail.com>
2018-09-06 00:10:47 +0700

    update

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AwesomeCMSCore.Modules.Entities.Enums;
+
+namespace AwesomeCMSCore.Modules.Admin.ViewModels
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public Guid UniqueId { get; set; }
+        public string Content { get; set; }
+        public CommentStatus CommentStatus { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/CommentViewModel.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/CommentViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AwesomeCMSCore.Modules.Entities.Entities;
-using AwesomeCMSCore.Modules.Entities.Enums;
+﻿using AwesomeCMSCore.Modules.Entities.Entities;
 using AwesomeCMSCore.Modules.Entities.ViewModel;
 
 namespace AwesomeCMSCore.Modules.Admin.ViewModels
@@ -12,14 +8,5 @@ namespace AwesomeCMSCore.Modules.Admin.ViewModels
         public Post Post { get; set; }
         public CommentDto Comment { get; set;}
         public UserViewModel User { get; set; }
-    }
-
-    public class CommentDto
-    {
-        public int Id { get; set; }
-        public Guid UniqueId { get; set; }
-        public string Content { get; set; }
-        public CommentStatus CommentStatus { get; set; }
-        public DateTime DateCreated { get; set; }
     }
 }
