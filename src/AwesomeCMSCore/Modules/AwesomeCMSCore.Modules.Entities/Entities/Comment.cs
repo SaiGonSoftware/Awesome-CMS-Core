966573e8df02cbb9f92b0f713b948d162371afde
Tony Hudson <ngohungphuc95@gmail.com>
2018-10-13 00:28:46 +0700

    update db design wih api to reply comment

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Entities/Entities/Comment.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Entities/Entities/Comment.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using AwesomeCMSCore.Modules.Entities.Enums;
 
 namespace AwesomeCMSCore.Modules.Entities.Entities
 {
-    public class Comment: BaseEntity
-    {
-        public string Content { get; set; }
-				public int ParentId { get; set; }
-        public Post Post { get; set; }
-        public User User { get; set; }
-        public CommentStatus CommentStatus { get; set; }
-    }
+	public class Comment : BaseEntity
+	{
+		public string Content { get; set; }
+		public Comment ParentComment { get; set; }
+		public Post Post { get; set; }
+		public User User { get; set; }
+		public CommentStatus CommentStatus { get; set; }
+	}
 }
