9b161623bb705d0bf6a9408851309709dd07f5d0
Tony Hudson <ngohungphuc@users.noreply.github.com>
2018-10-27 18:29:26 +0700

    ACC-refactor: refactor to hide sensitive user data (#261)

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/CommentViewModel.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/ViewModels/CommentViewModel.cs
@@ -1,11 +1,11 @@
-﻿using AwesomeCMSCore.Modules.Entities.Entities;
+using AwesomeCMSCore.Modules.Entities.Entities;
 using AwesomeCMSCore.Modules.Entities.ViewModel;
 
 namespace AwesomeCMSCore.Modules.Admin.ViewModels
 {
     public class CommentViewModel
     {
-        public Post Post { get; set; }
+        public PostViewModel Post { get; set; }
         public CommentDto Comment { get; set; }
         public UserViewModel User { get; set; }
     }
