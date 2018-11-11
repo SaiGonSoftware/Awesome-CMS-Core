6fefee314b55e728de88c7b46fa814435caebeb1
Tony Hudson <ngohungphuc95@gmail.com>
2018-11-11 22:16:44 +0700

    uupdate

--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
@@ -141,13 +141,13 @@ const PostContainer = props => {
 												<div className="widget widget-social-links wow fadeInUp">
 														<div className="widget-content">
 																<div className="widget-title">
-																		<h2>I'M social</h2>
+																		<h2>IM social</h2>
 																</div>
 																<div className="widget-extra-info-holder">
 																		<div className="widget-social-links">
 																				<ul className="social-links-list">
 																						<li className="facebook-link">
-																								<a href="http://facebook.com/" className="clearfix" target="_blank">
+																								<a href="http://facebook.com/" className="clearfix"  rel="noopener">
 																										Facebook
 																										<span className="social-icon">
 																												<i className="fa fa-facebook"></i>
@@ -155,7 +155,7 @@ const PostContainer = props => {
 																								</a>
 																						</li>
 																						<li className="twitter-link">
-																								<a href="http://twitter.com/" className="clearfix" target="_blank">
+																								<a href="http://twitter.com/" className="clearfix"  rel="noopener">
 																										Twitter
 																										<span className="social-icon">
 																												<i className="fa fa-twitter"></i>
@@ -163,7 +163,7 @@ const PostContainer = props => {
 																								</a>
 																						</li>
 																						<li className="googleplus-link">
-																								<a href="http://plus.google.com/" className="clearfix" target="_blank">
+																								<a href="http://plus.google.com/" className="clearfix"  rel="noopener">
 																										Google Plus
 																										<span className="social-icon">
 																												<i className="fa fa-google-plus"></i>
@@ -171,7 +171,7 @@ const PostContainer = props => {
 																								</a>
 																						</li>
 																						<li className="instagram-link">
-																								<a href="http://instagram.com/" className="clearfix" target="_blank">
+																								<a href="http://instagram.com/" className="clearfix"  rel="noopener">
 																										Instagram
 																										<span className="social-icon">
 																												<i className="fa fa-instagram"></i>
@@ -179,7 +179,7 @@ const PostContainer = props => {
 																								</a>
 																						</li>
 																						<li className="linkedin-link">
-																								<a href="http://linkedin.com/" className="clearfix" target="_blank">
+																								<a href="http://linkedin.com/" className="clearfix"  rel="noopener">
 																										Linked In
 																										<span className="social-icon">
 																												<i className="fa fa-linkedin"></i>
@@ -187,7 +187,7 @@ const PostContainer = props => {
 																								</a>
 																						</li>
 																						<li className="youtube-link">
-																								<a href="http://youtube.com/" className="clearfix" target="_blank">
+																								<a href="http://youtube.com/" className="clearfix"  rel="noopener">
 																										Youtube
 																										<span className="social-icon">
 																												<i className="fa fa-youtube"></i>
