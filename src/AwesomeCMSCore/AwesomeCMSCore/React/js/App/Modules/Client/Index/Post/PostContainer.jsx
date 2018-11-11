1a5c12844d2a647c982dbfe65fd2d837c914b830
Tony Hudson <ngohungphuc95@gmail.com>
2018-11-11 22:02:53 +0700

    update

--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
@@ -363,6 +363,101 @@ const PostContainer = props => {
 																</div>
 														</div>
 												</div>
+												<div className="widget widget-category wow fadeInUp">
+														<div className="widget-content">
+																<div className="widget-title">
+																		<h2>Category</h2>
+																</div>
+																<div className="widget-extra-info-holder">
+																		<ul className="widget-category-listings">
+																				<li>
+																						<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Lifestyle</a>
+																				</li>
+																				<li>
+																						<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Health</a>
+																				</li>
+																				<li>
+																						<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Article</a>
+																				</li>
+																				<li>
+																						<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Travel</a>
+																				</li>
+																				<li>
+																						<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Uncategorised</a>
+																				</li>
+																		</ul>
+																</div>
+														</div>
+												</div>
+												<div className="widget widget-popular-post wow fadeInUp">
+														<div className="widget-content">
+																<div className="widget-title">
+																		<h2>Trending Posts</h2>
+																</div>
+																<div className="widget-extra-info-holder">
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/one.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">That Evening At Bali Beach Was Wounderful Then Any Other Mornings</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>444
+																								<span>comments</span>
+																						</p>
+																				</div>
+																		</div>
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/five.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">5 Reasons Why Ladies Prefer To Have Brown Hair And Black Dress</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>345
+																								<span>comments</span>
+																						</p>
+																				</div>
+																		</div>
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/four.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">This post has just gone viral with many views</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>329
+																								<span>comments</span>
+																						</p>
+																				</div>
+																		</div>
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/two.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>256
+																								<span>comments</span>
+																						</p>
+																				</div>
+																		</div>
+																</div>
+														</div>
+												</div>
 										</div>
 								</aside>
 						</div>
