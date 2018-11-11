9a078a9e910b96a66bf1f081e5e01764bf3c219e
Tony Hudson <ngohungphuc95@gmail.com>
2018-11-11 22:03:43 +0700

    update

--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
@@ -458,6 +458,30 @@ const PostContainer = props => {
 																</div>
 														</div>
 												</div>
+												<div className="widget widget-newsletter wow fadeInUp">
+														<div className="widget-content">
+																<div className="widget-title">
+																		<h2>Newsletter</h2>
+																</div>
+																<div className="widget-extra-info-holder">
+																		<div className="widget-newsletter-content">
+																				<p>Subscribe to newsletter to be updated with all the latest trends and products.</p>
+																				<form>
+																						<div className="form-group">
+																								<input
+																										type="email"
+																										name="newsletter-email"
+																										placeholder="Email Address ..."
+																										className="form-control"/>
+																						</div>
+																						<div className="form-group">
+																								<button type="submit" name="submit" className="layout-one-btn">Subscribe</button>
+																						</div>
+																				</form>
+																		</div>
+																</div>
+														</div>
+												</div>
 										</div>
 								</aside>
 						</div>
