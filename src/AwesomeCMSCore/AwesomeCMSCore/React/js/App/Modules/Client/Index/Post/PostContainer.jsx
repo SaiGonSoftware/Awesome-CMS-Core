3a9a27d5a164a22cec4d3a274bbb4a6ab77e7ae5
Tony Hudson <ngohungphuc95@gmail.com>
2018-11-11 21:59:10 +0700

    update

--- src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
+++ src/AwesomeCMSCore/AwesomeCMSCore/React/js/App/Modules/Client/Index/Post/PostContainer.jsx
@@ -277,11 +277,79 @@ const PostContainer = props => {
 																</div>
 														</div>
 												</div>
+												<div className="widget widget-popular-post wow fadeInUp">
+														<div className="widget-content">
+																<div className="widget-title">
+																		<h2>Popular Posts</h2>
+																</div>
+																<div className="widget-extra-info-holder">
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/one.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">That Evening At Bali Beach Was Wounderful Then Any Other Mornings</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>1277
+																								<span>Views</span>
+																						</p>
+																				</div>
+																		</div>
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/five.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">5 Reasons Why Ladies Prefer To Have Brown Hair And Black Dress</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>865
+																								<span>Views</span>
+																						</p>
+																				</div>
+																		</div>
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/four.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">This post has just gone viral with many views</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>721
+																								<span>Views</span>
+																						</p>
+																				</div>
+																		</div>
+																		<div className="widget-posts">
+																				<div className="post-thumb">
+																						<img src="./Optimistic Blog Index_files/two.jpg" alt="....."/>
+																				</div>
+																				<div className="post-title">
+																						<h5>
+																								<a href="https://offshorethemes.com/html/optimistic-blog/demo/index-two.php#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+																						</h5>
+																				</div>
+																				<div className="post-view-count post-meta">
+																						<p>234
+																								<span>Views</span>
+																						</p>
+																				</div>
+																		</div>
+																</div>
+														</div>
+												</div>
 										</div>
 								</aside>
 						</div>
 				</div>
-
 		)
 }
 
