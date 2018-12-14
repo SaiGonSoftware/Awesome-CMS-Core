b757cc9fb533eeb2e0d264c2ad83065795aad851
Tony Hudson <ngohungphuc@users.noreply.github.com>
2018-12-14 13:18:03 +0700

    ACC-NetCore2.2 Upgrade to .Net Core 2.2 (#300)

--- src/AwesomeCMSCore/AwesomeCMSCore/Extension/ServiceCollectionExtensions.cs
+++ src/AwesomeCMSCore/AwesomeCMSCore/Extension/ServiceCollectionExtensions.cs
@@ -99,7 +99,7 @@ namespace AwesomeCMSCore.Extension
                 .AddJsonOptions(
                     options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
                 )
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+		        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddRazorOptions(o =>
                 {
                     foreach (var module in modules)
