3406a67284245d7372926f208d24ce23a5bfd6b0
ngohungphuc <ngohungphuc95@gmail.com>
2018-12-20 11:05:48 +0700

    add setting table

---
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Entities/Enums/SettingKey.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCMSCore.Modules.Entities.Enums
+{
+	public enum SettingKey
+	{
+		None = 0,
+		EmailSubscription = 1
+	}
+}
