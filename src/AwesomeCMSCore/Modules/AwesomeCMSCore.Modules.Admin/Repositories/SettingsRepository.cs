06a551c52ec9246e5d72c59985ec4301fe85fc1c
Tony Hudson <ngohungphuc@users.noreply.github.com>
2019-03-29 15:48:08 +0700

    small bug fix and add deployment script (#375)

--- src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/Repositories/SettingsRepository.cs
+++ src/AwesomeCMSCore/Modules/AwesomeCMSCore.Modules.Admin/Repositories/SettingsRepository.cs
@@ -83,7 +83,7 @@ namespace AwesomeCMSCore.Modules.Admin.Repositories
 		public async Task<SocialProfileSettings> GetSocialProfileSettings()
 		{
 			var result = await FindSocialProfileSettings();
-			var socialProfileSettings = JsonConvert.DeserializeObject<SocialProfileSettings>(result.Value);
+			var socialProfileSettings = result != null ?  JsonConvert.DeserializeObject<SocialProfileSettings>(result.Value) : null;
 			return socialProfileSettings;
 		}
 
